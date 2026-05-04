f947712d43b0e909854e2fdd1ecb1da8d4e2c017
09Hasan <hasn55667788jmal@gmail.com>
2026-05-04 15:51:53 +0300

    add package dartz create repo methods create file errors

--- lib/feature/home/data/repos/repo_home.dart
+++ lib/feature/home/data/repos/repo_home.dart
@@ -1,4 +1,8 @@
+import 'package:booklyapp/core/errors/failures.dart';
+import 'package:booklyapp/feature/home/data/models/model_book/model_book.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class RepoHome {
-  fetchBestSellerBooks();
-  fetchNewestBooks();
+  Future<Either<List<ModelBook>, Failures>> fetchBestSellerBooks();
+  Future<Either<List<ModelBook>, Failures>> fetchNewestBooks();
 }
